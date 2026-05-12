db86109387e138c067c85395762d880566fab913
MaxPichy <camilamachadodesouza77@gmail.com>
2026-05-11 22:41:13 -0300

    @MaxPichy

---
+++ aula08_bdmongo/exer_aula08/Passagem/Principal.py
@@ -0,0 +1,10 @@
+from Passagem import Passagem
+
+class Main:
+    @staticmethod
+    def main():
+        p = Passagem()
+        p.executar()
+        
+if __name__ == '__main__':
+    Main.main()
